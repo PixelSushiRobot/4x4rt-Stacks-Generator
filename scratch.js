e8efcb16216229c17f406046d7c174955d6cfa34
PixelSushiRobot <315162+PixelSushiRobot@users.noreply.github.com>
2026-05-10 12:09:24 -0700

    Fix poster layout borders and clean workspace files

--- scratch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('index.html', 'utf8');
-
-// I need to clean up the HTML at the end of artwork-container.
-const startIdx = code.indexOf('<div id="poster-metadata"');
-const endIdx = code.indexOf('</main>');
-console.log(code.substring(startIdx, endIdx));
